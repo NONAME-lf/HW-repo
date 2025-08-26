0c8e62c4557bc54edafafb5fb0a714872a5c1201
NONAME-lf <mekshakov.art@gmail.com>
2025-08-26 20:47:22 +0300

    HW 31 added info page

---
+++ 31-jquery/assets/js/main.js
@@ -0,0 +1,32 @@
+$(document).ready(() => {
+  /* As our donut is already initialized in html through the data atributes, 
+  here we'll have to manually create and insert the value of our chart into html */
+  const elem = document.getElementById("donutty");
+  elem.querySelector("svg circle:nth-child(3)").attributes[
+    "stroke-width"
+  ].value = 20;
+  //   Create and prepend custom gradient for stroke color
+  const defs = document.createElement("defs");
+  const lGradient = document.createElement("linearGradient");
+  lGradient.setAttribute("id", "grad");
+  lGradient.setAttribute("x1", "36.25%");
+  lGradient.setAttribute("y1", "98.05%");
+  lGradient.setAttribute("x2", "63.75%");
+  lGradient.setAttribute("y2", "1.95%");
+  lGradient.setAttribute("gradientUnits", "userSpaceOnUse");
+  lGradient.innerHTML = `<stop stop-color="#1AE780" offset=""></stop>
+    <stop offset="0.1875" stop-color="#00D4BE"></stop>
+    <stop offset="0.833333" stop-color="#00D4BE"></stop>
+    <stop offset="1" stop-color="#1AE780"></stop>`;
+  defs.append(lGradient);
+  const svg = document.querySelector("svg.donut").prepend(defs);
+
+  const chart = elem.donutty; // get alredy existant instance of our donut
+  chart.options.text = function (state) {
+    return state.value;
+  };
+  chart.createText(); // create span text
+  /* Because any initialization happens upon creation the only function that does it inserts all fragments,
+  so we must call it with given parameter */
+  chart.insertFragments(chart.getDashValues());
+});
