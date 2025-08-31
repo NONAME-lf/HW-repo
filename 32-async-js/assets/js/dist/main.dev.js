2d28dbf9ec7910df4c892fc790ea6e0de29b34cd
NONAME-lf <mekshakov.art@gmail.com>
2025-08-31 18:16:30 +0300

    HW 32 added details modals

--- 32-async-js/assets/js/dist/main.dev.js
+++ 32-async-js/assets/js/dist/main.dev.js
@@ -1,5 +1,15 @@
 "use strict";
 
+function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
+
+function _slicedToArray(arr, i) { return _arrayWithHoles(arr) || _iterableToArrayLimit(arr, i) || _nonIterableRest(); }
+
+function _nonIterableRest() { throw new TypeError("Invalid attempt to destructure non-iterable instance"); }
+
+function _iterableToArrayLimit(arr, i) { if (!(Symbol.iterator in Object(arr) || Object.prototype.toString.call(arr) === "[object Arguments]")) { return; } var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"] != null) _i["return"](); } finally { if (_d) throw _e; } } return _arr; }
+
+function _arrayWithHoles(arr) { if (Array.isArray(arr)) return arr; }
+
 var API_KEY = "fe91a24c";
 
 function searchMovies(search) {
@@ -47,9 +57,73 @@ function searchMovies(search) {
 function showMoviesList(movies) {
   var list = "";
   movies.forEach(function (movie) {
-    list += "\n    <div class=\"card\">\n  <img src=\"".concat(movie.Poster, "\" class=\"card-img-top\" alt=\"").concat(movie.Title, "\" onerror=\"this.src='assets/img/no-img.png'\">\n  <div class=\"card-body\">\n    <h5 class=\"card-title\">").concat(movie.Title, "</h5>\n    <p class=\"card-text\"><b>Year: </b>").concat(movie.Year, "</p>\n    <button class=\"btn btn-primary\">Details</a>\n  </div>\n</div>\n    ");
+    list += "\n    <div class=\"card movie-card\">\n  <img src=\"".concat(movie.Poster, "\" class=\"card-img-top\" alt=\"").concat(movie.Title, "\" onerror=\"this.src='assets/img/no-img.png'\">\n  <div class=\"card-body\">\n    <h5 class=\"card-title\">").concat(movie.Title, "</h5>\n    <p class=\"card-text\"><b>Year: </b>").concat(movie.Year, "</p>\n    <button data-movieid=\"").concat(movie.imdbID, "\" data-bs-toggle=\"modal\" data-bs-target=\"#movieModal\" class=\"btn btn-primary\">Details</a>\n  </div>\n</div>\n    ");
   });
   document.getElementById("movies-list").innerHTML = list;
+  document.querySelectorAll(".movie-card").forEach(function (movie) {
+    var button = movie.querySelector(".btn");
+    button.addEventListener("click", function (e) {
+      getMovieDetails(button.dataset.movieid);
+    });
+  });
+}
+
+function getMovieDetails(movie) {
+  var response, data;
+  return regeneratorRuntime.async(function getMovieDetails$(_context2) {
+    while (1) {
+      switch (_context2.prev = _context2.next) {
+        case 0:
+          _context2.next = 2;
+          return regeneratorRuntime.awrap(fetch("https://www.omdbapi.com/?apikey=".concat(API_KEY, "&i=").concat(movie)));
+
+        case 2:
+          response = _context2.sent;
+          _context2.next = 5;
+          return regeneratorRuntime.awrap(response.json());
+
+        case 5:
+          data = _context2.sent;
+          showMovieDetails(data);
+
+        case 7:
+        case "end":
+          return _context2.stop();
+      }
+    }
+  });
+}
+
+function showMovieDetails(movie) {
+  var details = "";
+  console.log(movie);
+
+  for (var _i = 0, _Object$entries = Object.entries(movie); _i < _Object$entries.length; _i++) {
+    var _Object$entries$_i = _slicedToArray(_Object$entries[_i], 2),
+        key = _Object$entries$_i[0],
+        value = _Object$entries$_i[1];
+
+    if (key === "imdbID") {
+      continue;
+    }
+
+    if (key === "Poster") {
+      details += "<b>".concat(key, ":</b> <a href=").concat(value, " target=\"_blank\">link</a><br>");
+      continue;
+    }
+
+    if (_typeof(value) === "object") {
+      details += "<b>".concat(key, ":</b><br>");
+      value.forEach(function (rating) {
+        details += "<b style=\"margin-left: 14%;\">".concat(Object.values(rating)[0], "</b>: ").concat(Object.values(rating)[1], "<br>");
+      });
+      continue;
+    }
+
+    details += "<b>".concat(key, ":</b> ").concat(value, "<br>");
+  }
+
+  document.querySelector(".modal-body").innerHTML = details;
 }
 
 var form = document.getElementById("search-form");
--- 32-async-js/assets/js/main.js
+++ 32-async-js/assets/js/main.js
@@ -18,17 +18,59 @@ function showMoviesList(movies) {
 
   movies.forEach((movie) => {
     list += `
-    <div class="card">
+    <div class="card movie-card">
   <img src="${movie.Poster}" class="card-img-top" alt="${movie.Title}" onerror="this.src='assets/img/no-img.png'">
   <div class="card-body">
     <h5 class="card-title">${movie.Title}</h5>
     <p class="card-text"><b>Year: </b>${movie.Year}</p>
-    <button class="btn btn-primary">Details</a>
+    <button data-movieid="${movie.imdbID}" data-bs-toggle="modal" data-bs-target="#movieModal" class="btn btn-primary">Details</a>
   </div>
 </div>
     `;
   });
   document.getElementById("movies-list").innerHTML = list;
+  document.querySelectorAll(".movie-card").forEach((movie) => {
+    const button = movie.querySelector(".btn");
+    button.addEventListener("click", function (e) {
+      getMovieDetails(button.dataset.movieid);
+    });
+  });
+}
+
+async function getMovieDetails(movie) {
+  const response = await fetch(
+    `https://www.omdbapi.com/?apikey=${API_KEY}&i=${movie}`
+  );
+
+  const data = await response.json();
+
+  showMovieDetails(data);
+}
+
+function showMovieDetails(movie) {
+  let details = "";
+  console.log(movie);
+
+  for (const [key, value] of Object.entries(movie)) {
+    if (key === "imdbID") {
+      continue;
+    }
+    if (key === "Poster") {
+      details += `<b>${key}:</b> <a href=${value} target="_blank">link</a><br>`;
+      continue;
+    }
+    if (typeof value === "object") {
+      details += `<b>${key}:</b><br>`;
+      value.forEach((rating) => {
+        details += `<b style="margin-left: 14%;">${
+          Object.values(rating)[0]
+        }</b>: ${Object.values(rating)[1]}<br>`;
+      });
+      continue;
+    }
+    details += `<b>${key}:</b> ${value}<br>`;
+  }
+  document.querySelector(".modal-body").innerHTML = details;
 }
 
 const form = document.getElementById("search-form");
