48e488e3ac6df4956d9ed1ecf2232962b8ec087d
NONAME-lf <mekshakov.art@gmail.com>
2025-09-03 16:50:23 +0300

    Exam 02 refactored moving gradient func

--- exam-2/assets/js/dist/main.dev.js
+++ exam-2/assets/js/dist/main.dev.js
@@ -2,35 +2,35 @@
 
 var w = 0;
 var h = 0;
-var defaultDirection = true;
-var bgDiv = document.querySelector(".background");
+var isForwardDirection = true;
+document.addEventListener("DOMContentLoaded", function () {
+  var bgDiv = document.querySelector(".background"); // Animates the background gradient by moving it diagonally in two directions,
+  // incrementing and decrementing the translation values to create a looping effect.
 
-function moveBgGradient(direction) {
-  if (w < 50 && defaultDirection) {
-    w += 10;
-    bgDiv.style.transform = "translate(".concat(-w, "%, ").concat(-h, "%)");
-    setTimeout(moveBgGradient, 500);
-  } else if (h < 50 && defaultDirection) {
-    h += 5;
-    bgDiv.style.transform = "translate(".concat(-w, "%, ").concat(-h, "%)");
-    setTimeout(moveBgGradient, 100);
-  } else if (w <= 50 && w > 0) {
-    defaultDirection = false;
-    w -= 5;
-    bgDiv.style.transform = "translate(".concat(-w, "%, ").concat(-h, "%)");
-    setTimeout(moveBgGradient, 500);
-  } else if (h <= 50 && h > 0) {
-    defaultDirection = false;
-    h -= 5;
-    bgDiv.style.transform = "translate(".concat(-w, "%, ").concat(-h, "%)");
-    setTimeout(moveBgGradient, 100);
-  } else {
-    defaultDirection = true;
-    w = 0;
-    h = 0;
+  function moveBgGradient() {
+    if (!bgDiv) return;
+
+    if (isForwardDirection) {
+      if (w < 50) {
+        w += 5;
+      } else if (h < 50) {
+        h += 5;
+      } else {
+        isForwardDirection = false;
+      }
+    } else {
+      if (w > 0) {
+        w -= 5;
+      } else if (h > 50) {
+        h -= 5;
+      } else {
+        isForwardDirection = true;
+      }
+    }
+
     bgDiv.style.transform = "translate(".concat(-w, "%, ").concat(-h, "%)");
     setTimeout(moveBgGradient, 500);
   }
-}
 
-moveBgGradient();
+  moveBgGradient();
+});
--- exam-2/assets/js/main.js
+++ exam-2/assets/js/main.js
@@ -1,33 +1,31 @@
 let w = 0;
 let h = 0;
-let defaultDirection = true;
-const bgDiv = document.querySelector(".background");
-function moveBgGradient(direction) {
-  if (w < 50 && defaultDirection) {
-    w += 10;
-    bgDiv.style.transform = `translate(${-w}%, ${-h}%)`;
-    setTimeout(moveBgGradient, 500);
-  } else if (h < 50 && defaultDirection) {
-    h += 5;
-    bgDiv.style.transform = `translate(${-w}%, ${-h}%)`;
-    setTimeout(moveBgGradient, 100);
-  } else if (w <= 50 && w > 0) {
-    defaultDirection = false;
-    w -= 5;
-    bgDiv.style.transform = `translate(${-w}%, ${-h}%)`;
-    setTimeout(moveBgGradient, 500);
-  } else if (h <= 50 && h > 0) {
-    defaultDirection = false;
-    h -= 5;
-    bgDiv.style.transform = `translate(${-w}%, ${-h}%)`;
-    setTimeout(moveBgGradient, 100);
-  } else {
-    defaultDirection = true;
-    w = 0;
-    h = 0;
+let isForwardDirection = true;
+document.addEventListener("DOMContentLoaded", function () {
+  const bgDiv = document.querySelector(".background");
+  // Animates the background gradient by moving it diagonally in two directions,
+  // incrementing and decrementing the translation values to create a looping effect.
+  function moveBgGradient() {
+    if (!bgDiv) return;
+    if (isForwardDirection) {
+      if (w < 50) {
+        w += 5;
+      } else if (h < 50) {
+        h += 5;
+      } else {
+        isForwardDirection = false;
+      }
+    } else {
+      if (w > 0) {
+        w -= 5;
+      } else if (h > 50) {
+        h -= 5;
+      } else {
+        isForwardDirection = true;
+      }
+    }
     bgDiv.style.transform = `translate(${-w}%, ${-h}%)`;
     setTimeout(moveBgGradient, 500);
   }
-}
-
-moveBgGradient();
+  moveBgGradient();
+});
