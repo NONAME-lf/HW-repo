b06acb242a0dde661b90284226b7ac6839c4c97b
NONAME-lf <mekshakov.art@gmail.com>
2025-09-06 00:34:53 +0300

    Exam 02 added what we do section
    Also removed light slider from wraping all the sections and replaced it with href="#X"

--- exam-2/assets/js/dist/main.dev.js
+++ exam-2/assets/js/dist/main.dev.js
@@ -40,12 +40,49 @@ document.addEventListener("scroll", function (e) {
   var header = document.querySelector("header");
   var headerBg = document.querySelector(".header-background");
   var windowScroll = window.scrollY;
-  var heroHeight = hero.clientHeight; // Move header proportional to scroll up to 0y
+  var heroHeight = hero.clientHeight;
+  var whatWeDoHeight = document.querySelector(".what-we-do").clientHeight + heroHeight;
+  var latestNewsHeight = document.querySelector(".latest-news").clientHeight + whatWeDoHeight;
+  var galleryHeight = document.querySelector(".gallery").clientHeight + latestNewsHeight;
+  var contactHeight = document.querySelector(".contact").clientHeight + galleryHeight; // Move header proportional to scroll up to 0y
 
   if (windowScroll <= 41) header.style.top = "".concat(41 - windowScroll, "px");else header.style.top = "0px"; // Blur header if scroll is proportionaly close to the hero section content
 
-  if (windowScroll > heroHeight * 0.1) header.classList.add("blur");else header.classList.remove("blur");
+  if (windowScroll > heroHeight * 0.1) {
+    header.classList.add("blur");
+    document.querySelector(".pager").classList.add("pager-bg");
+  } else {
+    header.classList.remove("blur");
+    document.querySelector(".pager").classList.remove("pager-bg");
+  }
+
   if (windowScroll > heroHeight - header.clientHeight) headerBg.style.opacity = 0.7;else document.querySelector(".header-background").style.opacity = 0;
+
+  switch (true) {
+    case windowScroll < heroHeight:
+      togglePagerClass(document.getElementById("hero-pager"));
+      break;
+
+    case windowScroll > heroHeight && windowScroll < whatWeDoHeight:
+      togglePagerClass(document.getElementById("what-we-do-pager"));
+      break;
+
+    case windowScroll > whatWeDoHeight && windowScroll < latestNewsHeight:
+      togglePagerClass(document.getElementById("news-pager"));
+      break;
+
+    case windowScroll > latestNewsHeight && windowScroll < galleryHeight:
+      togglePagerClass(document.getElementById("gallery-pager"));
+      break;
+
+    case windowScroll > galleryHeight && windowScroll < contactHeight:
+      togglePagerClass(document.getElementById("contact-pager"));
+      break;
+
+    default:
+      togglePagerClass(document.getElementById("hero-pager"));
+      break;
+  }
 });
 var toggle = -1;
 var isHover = false; // function to point out an arrow
@@ -76,3 +113,15 @@ function jumpingArrow() {
 }
 
 jumpingArrow();
+var dots = document.querySelectorAll(".pager a");
+dots.forEach(function (element) {
+  element.addEventListener("click", function (e) {
+    togglePagerClass(e.target);
+  });
+});
+
+function togglePagerClass(element) {
+  var parent = element.parentElement;
+  document.querySelector(".active").classList.remove("active");
+  parent.classList.add("active");
+}
