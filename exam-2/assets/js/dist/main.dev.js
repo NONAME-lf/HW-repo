3b930ea83d7a8182bc7fa199b7a0ba3af3ad24c9
NONAME-lf <mekshakov.art@gmail.com>
2025-09-05 19:45:28 +0300

    Exam 02 finished header / hero & added slider

--- exam-2/assets/js/dist/main.dev.js
+++ exam-2/assets/js/dist/main.dev.js
@@ -4,8 +4,8 @@ var w = 0;
 var h = 0;
 var isForwardDirection = true;
 document.addEventListener("DOMContentLoaded", function () {
-  var bgDiv = document.querySelector(".background"); // Animates the background gradient by moving it diagonally in two directions,
-  // incrementing and decrementing the translation values to create a looping effect.
+  var bgDiv = document.querySelector(".background"); // Function that moves the gradient background,
+  // by changing the translation values after a period of time
 
   function moveBgGradient() {
     if (!bgDiv) return;
@@ -33,4 +33,46 @@ document.addEventListener("DOMContentLoaded", function () {
   }
 
   moveBgGradient();
+}); // Event listener for fixed(sticky) header
+
+document.addEventListener("scroll", function (e) {
+  var hero = document.querySelector(".hero-section");
+  var header = document.querySelector("header");
+  var headerBg = document.querySelector(".header-background");
+  var windowScroll = window.scrollY;
+  var heroHeight = hero.clientHeight; // Move header proportional to scroll up to 0y
+
+  if (windowScroll <= 41) header.style.top = "".concat(41 - windowScroll, "px");else header.style.top = "0px"; // Blur header if scroll is proportionaly close to the hero section content
+
+  if (windowScroll > heroHeight * 0.1) header.classList.add("blur");else header.classList.remove("blur");
+  if (windowScroll > heroHeight - header.clientHeight) headerBg.style.opacity = 0.7;else document.querySelector(".header-background").style.opacity = 0;
 });
+var toggle = -1;
+var isHover = false; // function to point out an arrow
+
+function jumpingArrow() {
+  var arrow = document.getElementById("arrow"); // pause on hover
+
+  arrow.addEventListener("mouseover", function () {
+    isHover = true;
+  });
+  arrow.addEventListener("mouseleave", function () {
+    isHover = false;
+  });
+
+  if (toggle < 2 && !isHover) {
+    if (arrow.style.bottom == "49px") {
+      arrow.style.bottom = "10px";
+    } else {
+      arrow.style.bottom = "49px";
+    }
+
+    toggle++;
+    setTimeout(jumpingArrow, 75);
+  } else {
+    toggle = 0;
+    setTimeout(jumpingArrow, 3000);
+  }
+}
+
+jumpingArrow();
--- exam-2/assets/js/main.js
+++ exam-2/assets/js/main.js
@@ -3,8 +3,8 @@ let h = 0;
 let isForwardDirection = true;
 document.addEventListener("DOMContentLoaded", function () {
   const bgDiv = document.querySelector(".background");
-  // Animates the background gradient by moving it diagonally in two directions,
-  // incrementing and decrementing the translation values to create a looping effect.
+  // Function that moves the gradient background,
+  // by changing the translation values after a period of time
   function moveBgGradient() {
     if (!bgDiv) return;
     if (isForwardDirection) {
@@ -29,3 +29,52 @@ document.addEventListener("DOMContentLoaded", function () {
   }
   moveBgGradient();
 });
+
+// Event listener for fixed(sticky) header
+document.addEventListener("scroll", (e) => {
+  const hero = document.querySelector(".hero-section");
+  const header = document.querySelector("header");
+  const headerBg = document.querySelector(".header-background");
+  const windowScroll = window.scrollY;
+  const heroHeight = hero.clientHeight;
+
+  // Move header proportional to scroll up to 0y
+  if (windowScroll <= 41) header.style.top = `${41 - windowScroll}px`;
+  else header.style.top = `0px`;
+
+  // Blur header if scroll is proportionaly close to the hero section content
+  if (windowScroll > heroHeight * 0.1) header.classList.add("blur");
+  else header.classList.remove("blur");
+
+  if (windowScroll > heroHeight - header.clientHeight)
+    headerBg.style.opacity = 0.7;
+  else document.querySelector(".header-background").style.opacity = 0;
+});
+
+let toggle = -1;
+let isHover = false;
+// function to point out an arrow
+function jumpingArrow() {
+  const arrow = document.getElementById("arrow");
+  // pause on hover
+  arrow.addEventListener("mouseover", () => {
+    isHover = true;
+  });
+  arrow.addEventListener("mouseleave", () => {
+    isHover = false;
+  });
+  if (toggle < 2 && !isHover) {
+    if (arrow.style.bottom == "49px") {
+      arrow.style.bottom = "10px";
+    } else {
+      arrow.style.bottom = "49px";
+    }
+    toggle++;
+    setTimeout(jumpingArrow, 75);
+  } else {
+    toggle = 0;
+    setTimeout(jumpingArrow, 3000);
+  }
+}
+
+jumpingArrow();
