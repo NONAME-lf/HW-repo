7a8bd8ec6b95b9b44e86da04d321c33c24d47f30
NONAME-lf <mekshakov.art@gmail.com>
2025-09-10 16:07:10 +0300

    Exam 02 changed  gitignore

---
+++ exam-2/assets/js/dist/keys.dev.js
@@ -0,0 +1,10 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.CHAT_ID = exports.BOT_TOKEN = void 0;
+var BOT_TOKEN = "7951225413:AAGje8ftyap6ekTfHz-K-oQGLZMyQ9g7__o";
+exports.BOT_TOKEN = BOT_TOKEN;
+var CHAT_ID = "-4965449774";
+exports.CHAT_ID = CHAT_ID;
---
+++ exam-2/assets/js/keys.js
@@ -0,0 +1,2 @@
+export const BOT_TOKEN = "7951225413:AAGje8ftyap6ekTfHz-K-oQGLZMyQ9g7__o";
+export const CHAT_ID = "-4965449774";
